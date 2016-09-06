0fe34eaf30b2faf775b3a540f0674c8bf633f2f8
Janakiram MSV <janakiramm@gmail.com>
2016-09-06 10:27:34 +0530

    Added Setup Scripts

---
+++ setup.sh
@@ -0,0 +1,8 @@
+python -m pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+
+python -m pip install grpcio
+python -m pip install grpcio-tools
+
